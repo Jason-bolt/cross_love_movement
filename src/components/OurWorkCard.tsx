6f2aa95e7bc840bd1af96fc88990b2359575c957
Jason Appiatu <appiatujason@gmail.com>
2025-04-01 01:27:40 +0000

    Fix: Corrected community pic height

--- src/components/OurWorkCard.tsx
+++ src/components/OurWorkCard.tsx
@@ -9,7 +9,7 @@ const OurWorkCard = ({ imageUrl, name, link }: IOurWorkCard) => {
     <div className="flex flex-col items-center justify-start w-72 h-86 bg-amber-50 rounded-lg shadow-lg hover:shadow-xl">
       {/* <div className={`bg-[url(${imageUrl})] bg-center bg-no-repeat bg-cover w-full h-70`}></div> */}
       <div className="w-full h-70 overflow-hidden">
-        <img src={imageUrl} alt={name} className="object-fill" />
+        <img src={imageUrl} alt={name} className="object-cover w-full h-full" />
       </div>
       <div className="flex flex-col items-center justify-center p-4">
         <p className="text-amber-900 text-2xl font-bold">{name}</p>
